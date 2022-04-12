f424e6b148d0bdd886ca8fc086a028d4bd07dd30
FiPiL99 <fipis14@gmail.com>
2022-04-13 01:54:02 +0200

    JoinRoom and RoomCreations headers

--- Cards/client-side/my-app/src/components/Lobby/JoinRoom.tsx
+++ Cards/client-side/my-app/src/components/Lobby/JoinRoom.tsx
@@ -11,26 +11,30 @@ function JoinRoom({joinRoom, rooms} : {joinRoom : any, rooms: RoomModel[]}) {
     let roomName : string;
     let roomId  : string;
     return (
-        <Form className="hubs-container"
-            onSubmit={e => {
-                e.preventDefault();
-                const userModel = new UserModel();
-                userModel.name = user;
+        <div>
+            <h2>Join Room</h2>
+            <Form className="hubs-container"
+                onSubmit={e => {
+                    e.preventDefault();
+                    const userModel = new UserModel();
+                    userModel.name = user;
 
-                joinRoom(userModel, roomId);
-                history.push(`room/${roomName}`)
-            }}
-        >
-            <Form.Control placeholder="name" onChange={e => setUser(e.target.value)} />
-            {
-                rooms.map((element, index) => 
-                        (<div key={index}>
-                            <h3>{element.roomName}</h3>
-                            <Button variant="success" type="submit" disabled={!user} onClick={() => {roomName=element.roomName; roomId = element.id}}>Join</Button>
-                        </div>
-                    )
-            )}
-        </Form>
+                    joinRoom(userModel, roomId);
+                    history.push(`room/${roomName}`)
+                }}
+            >
+                <Form.Control placeholder="name" onChange={e => setUser(e.target.value)} />
+                {
+                    rooms.map((element, index) => 
+                            (<div key={index}>
+                                <h3>{element.roomName}</h3>
+                                <Button variant="success" type="submit" disabled={!user} onClick={() => {roomName=element.roomName; roomId = element.id}}>Join</Button>
+                            </div>
+                        )
+                )}
+            </Form> 
+        </div>
+   
     );
 }
 
--- Cards/client-side/my-app/src/components/Lobby/RoomCreation.tsx
+++ Cards/client-side/my-app/src/components/Lobby/RoomCreation.tsx
@@ -7,19 +7,23 @@ function RoomCreation({ createRoom, user}: {createRoom : any, user : UserModel})
     const [room, setRoom] = useState('');
     const history = useHistory();
     return (
-        <Form className="lobby"
-            onSubmit={e => {
-                e.preventDefault();
-                createRoom(user, room);
-                history.push(`room/${room}`)
-            }}
-        >
-            <Form.Group>
-                <Form.Control placeholder="name" onChange={e => user.name = e.target.value} />
-                <Form.Control placeholder="room" onChange={e => setRoom(e.target.value)} />
-            </Form.Group>
-            <Button variant="success" type="submit" disabled={!user || !room }>Join</Button>
-        </Form>
+         <div>
+            <h2>Create Room</h2>
+            <Form className="lobby"
+                onSubmit={e => {
+                    e.preventDefault();
+                    createRoom(user, room);
+                    history.push(`room/${room}`)
+                }}
+            >
+                <Form.Group>
+                    <Form.Control placeholder="name" onChange={e => user.name = e.target.value} />
+                    <Form.Control placeholder="room" onChange={e => setRoom(e.target.value)} />
+                </Form.Group>
+                <Button variant="success" type="submit" disabled={!user || !room }>Join</Button>
+            </Form>
+         </div>
+
     );
 }
 export default RoomCreation;
